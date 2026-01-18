abe327721a6246a8e21ef2463a1de46b27ec3ba1
dave-notch <stsaamir86@gmail.com>
2026-01-18 17:14:23 +0300

    logic problems

--- app.js
+++ app.js
@@ -1,4 +1,97 @@
-const toggle = document.querySelector(".menu-toggle");
+  let nameHapit=document.getElementById("nameHapit");
+  let habitName=document.getElementById("habitName");
+  let option_select=document.getElementById("option_select");
+  let dateDisplay=document.getElementById("dateDisplay");
+  let submit=document.getElementById("Submit");
+  let showResults=document.getElementById("showResults");
+  let goalDisplay=document.getElementById("goalDisplay");
+  let daysGoal=0;
+
+  
+
+ 
+ let intervalStarted = false;
+
+  submit.addEventListener("click",(event)=>{
+    event.preventDefault()
+     let hapit=nameHapit.value
+        if(!hapit){
+          habitName.innerHTML=""
+          
+        }else{
+          habitName.innerHTML=hapit
+        }
+     
+
+    let showDate=date.value
+        if(!showDate){
+          dateDisplay.innerHTML=""
+          
+        }else{
+          dateDisplay.innerHTML=showDate
+        }
+
+    let spent=Amount_Input.value
+        if(!spent){
+          SpentDisplay.innerHTML=""
+          
+        }else{
+          SpentDisplay.innerHTML=`$ ${spent}`
+        }
+   
+
+        daysGoal=goalSet.value
+        if(!daysGoal){
+          goalDisplay.innerHTML=""
+        }else{
+          goalDisplay.innerHTML=`${daysGoal} Days`
+          RMdays.innerHTML=daysGoal
+        }
+        remainingCounter(daysGoal)
+    
+
+
+    let select=habit_select.value
+        if(select==""){
+          option_select.innerHTML=""
+          
+        }else{
+          option_select.innerHTML=select
+        }
+    
+  if(daysGoal  && showDate && hapit){
+     if(!intervalStarted) {
+        setInterval(updateSeconds, 1000);
+        setInterval(remainingCounter, 1)
+        intervalStarted = true; 
+     }
+  }
+
+    })
+    
+    
+    
+  
+
+
+
+
+
+
+//  showResults.addEventListener("click", ()=>{
+//   resultArea.style.display="block"
+//   form_add.style.display="none"
+  
+
+
+//  })
+
+
+
+
+
+
+  const toggle = document.querySelector(".menu-toggle");
   const links = document.querySelector(".nav-links");
   const AmountSpent=document.getElementById("AmountSpent");
   const noCheck=document.getElementById("noCheck");
@@ -49,38 +142,35 @@ const toggle = document.querySelector(".menu-toggle");
   // })
   let startingSeconds=0;
   let time=startingSeconds;
-  let RMsecoCounter=60;
+
+
+  
+
   let mincounter=0;
-  let RMmincounter=59;
   let hourCounter=0;
-  let RMhourCounter=23;
   let dayCounter=0;
-  let RDcounter;
   let goalSet=document.getElementById("goalSet");
 
   function updateSeconds(){
      if (time < 10) {
       seconds.innerHTML = "0" + time;
-      RMseconds.innerHTML = "0" + RMsecoCounter;
     } else {
       seconds.innerHTML = time;
-      RMseconds.innerHTML = RMsecoCounter;
     }
+    
+    
     
     time+=1;
     if (mincounter < 10) {
-      minutes.innerHTML = "0" + mincounter;
-      RMminutes.innerHTML = "0" + RMmincounter;
+      minutes.innerHTML = "0" + mincounter;  
     } else {
       minutes.innerHTML = mincounter;
-      RMminutes.innerHTML = RMmincounter;
     }
      if (hourCounter < 10) {
       hours.innerHTML = "0" + hourCounter;
-      RMhours.innerHTML = "0" + RMhourCounter;
+
     } else {
       hours.innerHTML = hourCounter;
-      RMhours.innerHTML = RMhourCounter;
     }
 
     
@@ -89,31 +179,24 @@ const toggle = document.querySelector(".menu-toggle");
     if(time==60){
       time=startingSeconds
       mincounter+=1
-      RMmincounter-=1
       if (mincounter < 10) {
       minutes.innerHTML = "0" + mincounter;
-      RMminutes.innerHTML = "0" + RMmincounter;
     } else {
       minutes.innerHTML = mincounter;
-      RMminutes.innerHTML = RMmincounter;
     }
-    if(mincounter==59){
-      mincounter=0
-      RMmincounter=59
-      hourCounter+=1
-      RMhourCounter-=1
-      
-    }
+
     if (hourCounter < 10) {
       hours.innerHTML = "0" + hourCounter;
-      RMhours.innerHTMl="0" + RMhourCounter;
     } else {
       hours.innerHTML = hourCounter;
-      RMhours.innerHTML = RMhourCounter;
+    }
+
+    if(mincounter==59){
+      mincounter=0
+      hourCounter+=1
     }
     if(hourCounter==23){
       hourCounter=0;
-      RMhourCounter=23
       dayCounter+=1;
 
     if (dayCounter < 10) {
@@ -131,97 +214,72 @@ const toggle = document.querySelector(".menu-toggle");
     }
     }
 
-
-
-  }
-
-
-  let nameHapit=document.getElementById("nameHapit");
-  let habitName=document.getElementById("habitName");
-  let option_select=document.getElementById("option_select");
-  let dateDisplay=document.getElementById("dateDisplay");
-  let submit=document.getElementById("Submit");
-  let showResults=document.getElementById("showResults");
-  let goalDisplay=document.getElementById("goalDisplay");
-
-  
-
- 
- let intervalStarted = false;
-
-  submit.addEventListener("click",(event)=>{
-    event.preventDefault()
-     let hapit=nameHapit.value
-        if(!hapit){
-          habitName.innerHTML=""
-          
-        }else{
-          habitName.innerHTML=hapit
-        }
-     
-
-    let showDate=date.value
-        if(!showDate){
-          dateDisplay.innerHTML=""
-          
-        }else{
-          dateDisplay.innerHTML=showDate
-        }
-
-    let spent=Amount_Input.value
-        if(!spent){
-          SpentDisplay.innerHTML=""
-          
-        }else{
-          SpentDisplay.innerHTML=`$ ${spent}`
-        }
    
 
-    let daysGoal=goalSet.value
-        if(!daysGoal){
-          goalDisplay.innerHTML=""
-        }else{
-          goalDisplay.innerHTML=`${daysGoal} Days`
-          RMdays.innerHTML=daysGoal
-        }
-    
-
-
-    let select=habit_select.value
-        if(select==""){
-          option_select.innerHTML=""
-          
-        }else{
-          option_select.innerHTML=select
-        }
-    
-  if(daysGoal  && showDate && hapit){
-     if(!intervalStarted) {
-        setInterval(updateSeconds, 1000);
-        intervalStarted = true; 
-     }
   }
 
-    })
+  let RMsecoCounter=60;
+  let secs=RMsecoCounter
+  let RMmincounter=5;
+  let mins=RMmincounter
+  let RMhourCounter=5;
+  let HRS=RMhourCounter
+
+
+  function remainingCounter(){
+    if (RMsecoCounter > 10) {
+      RMseconds.innerHTML = secs;
+    } else {
+      RMseconds.innerHTML = "0" + secs;
+    }
+
+    secs-=1
+  RMminutes.innerHTML = mins;
+  RMhours.innerHTML = HRS;
+
+
+
+    if(secs==0){
+      secs=RMsecoCounter
+      mins-=1
+
     
-    
-    
+
+    if(mins==0){
+      mins=RMmincounter
+      HRS-=1
+    }
+
+    if(HRS==0){
+      HRS=RMhourCounter
+      daysGoal-=1
+      // RMhourCounter-=1
+    }
+
+
+
+
+    if (mins > 10) {
+      RMminutes.innerHTML = mins;
+    } else {
+      RMminutes.innerHTML = "0" + mins;
+    }
+
+    if (HRS > 10) {
+      RMhours.innerHTML = HRS;
+    } else {
+      RMhours.innerHTML = "0" + HRS;
+    }
+
+     if (daysGoal > 10) {
+      goalDisplay.innerHTML = daysGoal;
+    } else {
+      goalDisplay.innerHTML = "0" + daysGoal;
+    }
+
+  }
+
+  }
+
+
   
-
-
-
-
-
-
-//  showResults.addEventListener("click", ()=>{
-//   resultArea.style.display="block"
-//   form_add.style.display="none"
-  
-
-
-//  })
-
-
-
-
-
